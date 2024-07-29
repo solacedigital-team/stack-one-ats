21a90b938349b25724c7a42685e229a31293182f
anuragkainth <anuragkainth07@gmail.com>
2024-07-29 17:15:43 +0530

    update: added list all jobs & appli components

--- backend/src/controllers/connectSession.ts
+++ backend/src/controllers/connectSession.ts
@@ -26,13 +26,32 @@ export const retrieveConnectSessionToken = async (req: Request, res: Response): 
   }
 };
 
-export const fetchAllJobs = async (req: Request, res: Response): Promise<void> => {
-
+export const listAllJobs = async (req: Request, res: Response): Promise<void> => {
   const accountId = req.headers['x-account-id'];
   console.log("backend contoller")
   
   try {
-    const response = await axios.get('https://api.stackone.com/unified/ats/jobs', {
+    const response = await axios.get('https://api.stackone.com/unified/ats/job_postings', { // ENV Var url, to be changed
+      headers: {
+        'accept': 'application/json',
+        'x-account-id': `${accountId}`,
+        'authorization': `Basic ${config.STACKONE_API_KEY}`,
+      }
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching jobs from StackOne', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+export const listApplications = async (req: Request, res: Response): Promise<void> => {
+  const accountId = req.headers['x-account-id'];
+  console.log("backend contoller")
+  
+  try {
+    const response = await axios.get('https://api.stackone.com/unified/ats/applications', { // ENV Var url, to be changed
       headers: {
         'accept': 'application/json',
         'x-account-id': `${accountId}`,
--- backend/src/routes/connectSession.ts
+++ backend/src/routes/connectSession.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { retrieveConnectSessionToken, fetchAllJobs } from '../controllers/connectSession';
+import { retrieveConnectSessionToken, listAllJobs, listApplications } from '../controllers/connectSession';
 
 const router = express.Router();
 
 router.post('/connect_session', retrieveConnectSessionToken);
-router.get('/jobs', fetchAllJobs);
+router.get('/job_postings', listAllJobs);
+router.get('/list_applications', listApplications);
 
 export default router;
